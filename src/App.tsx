d835c64f484c7440e4d75e758773a36ba435de55
Kelvin Bush <kelybush@gmail.com>
2021-11-17 20:38:42 +0300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 //routing
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //components
 import Header from "./components/Header";
 import Home from "./components/Home";
 
 //styles
-import {GlobalStyle} from "./GlobalStyle";
+import { GlobalStyle } from "./GlobalStyle";
 import Movie from "./Movie";
 import NotFound from "./components/NotFound";
+import UserProvider from "./context";
 
 const App: React.FC = () => (
-    <Router>
-        <Header/>
-        <Routes>
-            <Route path={"/"} element={<Home/>}/>
-            <Route path={"/:movieId"} element={<Movie/>}/>
-            <Route path={"/*"} element={<NotFound/>}/>
-        </Routes>
-        <GlobalStyle/>
-    </Router>
+  <Router>
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/:movieId"} element={<Movie />} />
+        <Route path={"/*"} element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
+  </Router>
 );
 
 export default App;
